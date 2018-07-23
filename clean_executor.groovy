58e7c059a0814430e7a6b659beafd651fd1da86a
Vinood Katuwa <vinod.katuwa12@gmail.com>
2018-07-23 18:51:31 +0530

    Create clean_executor.groovy

---
+++ clean_executor.groovy
@@ -0,0 +1,37 @@
+public int cancelRunning() {
+ // Cancel running builds.
+ def numCancels = 0;
+ for (job in this.hudson.instance.items) {
+  for (build in job.builds) {
+   if (build == this.build) {
+    continue;
+   } // don't cancel ourself!
+   if (!build.hasProperty('causes')) {
+    continue;
+   }
+   if (!build.isBuilding()) {
+    continue;
+   }
+   for (cause in build.causes) {
+    if (!cause.hasProperty('upstreamProject')) {
+     continue;
+    }
+    if (cause.upstreamProject == this.upstreamProject &&
+     cause.upstreamBuild == this.upstreamBuild) {
+     this.printer.println('Stopping ' + build.toString());
+     build.doStop();
+     this.printer.println(build.toString() + ' stopped.');
+     numCancels++;
+     break;
+    }
+   }
+  }
+ }
+ return numCancels;
+}
+
+import hudson.model.*
+ def q = Jenkins.instance.queue
+q.items.each {
+ q.cancel(it.task)
+}
